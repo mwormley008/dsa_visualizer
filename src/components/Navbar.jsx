177cb17516f459d0736abd6282c8708bd56ecf2b
mwormley008 <mwormley008@gmail.com>
2023-04-16 16:06:56 -0500

    text on screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import React from 'react';
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
+            <div className="container-fluid border-bottom border-3">
+                <h1>DSA Visualizer</h1>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
--- src/components/lnode.jsx
+++ src/components/lnode.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 
 export default function Lnode() {
     return (
-        <div className="card mt-3 success mx-auto w-50">
-            <div className="row g-0">
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h1>hi</h1>
+        <div className="card mt-3 success mx-auto col-6">
+            <div className="slnode row g-0">
+                    <div className="data card-body border border-3 col-md-3 border-end">
+                        <h1 className="text-center">Data</h1>
                     </div>
-                </div>
+                    <div className="pointer col-md-3 border border-3 card-body">
+                        <h1 className="text-center">Data</h1>
+                    </div>
             </div>
         </div>
     )

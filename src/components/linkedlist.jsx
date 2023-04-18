71f72c7f8e1fa9d24e85a4b77bc6e33a18eef2b9
mwormley008 <mwormley008@gmail.com>
2023-04-18 16:58:59 -0500

    delete by index

--- src/components/linkedlist.jsx
+++ src/components/linkedlist.jsx
@@ -5,6 +5,8 @@ import Arrow from './arrow';
 
 export default function LinkedList(){
   const [nodes, setNodes] = useState([])
+  const [inputValue, setInputValue] = useState('');
+
   function addNode(e){
       setNodes([...nodes, { id: nodes.length + 1, value: Math.round(Math.random() *10) } ]);
   };
@@ -14,13 +16,26 @@ export default function LinkedList(){
         return nodes.filter(node => node.id !== nodes.length)
       })
   }
-  
+
+  function delIdx(e){
+    const index = parseInt(inputValue);
+    if (!isNaN(index) && index >= 1 && index <= nodes.length) {
+      setNodes(nodes.filter(node => node.id !== index));
+    }
+    setInputValue('');
+  }
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
 
   return ( 
       <div className="container">
         <div className="row justify-content-between">
         <button onClick={addNode}>Add Node</button>
         <button onClick={delNode}>Delete Node</button>
+        <input type="text" value={inputValue} onChange={handleInputChange}/>
+        <button onClick={delIdx}>Delete Index</button>
       { nodes.map( node => (
         <> 
           <Lnode key={node.id} value={node.value} /> 

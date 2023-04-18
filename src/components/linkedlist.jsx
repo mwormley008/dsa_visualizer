b7b63e9f0ded9e4a53e30d98b562149c7a51391a
mwormley008 <mwormley008@gmail.com>
2023-04-18 15:05:54 -0500

    same random numbers in nodes

--- src/components/linkedlist.jsx
+++ src/components/linkedlist.jsx
@@ -2,29 +2,23 @@ import React, { useState } from 'react';
 import Lnode from './lnode';
 import Arrow from './arrow';
 
-export default function LinkedList() {
 
-  const [count, setCount] = useState(0)
+export default function LinkedList(){
+  const [nodes, setNodes] = useState([])
+  function handleClick(e){
+      setNodes([...nodes, { id: nodes.length + 1, value: Math.round(Math.random() *10) } ]);
+  };
 
-  let nodes = []
-  for(let i = 0; i<count; i++) {
-    nodes.push(
-      <> 
-      <Lnode /><Arrow /> 
-      </>
-    )
-  }
-
-  function handleClick (e) {
-    setCount(count+1)
-  }
-
-  return (
-    <div className="container">
-      <div className="row justify-content-between">
+  return ( 
+      <div className="container">
+        <div className="row justify-content-between">
         <button onClick={handleClick}>Add Node</button>
-        {nodes}
-      </div>
-    </div>
-  )
+      { nodes.map( node => (
+        <> 
+          <Lnode key={node.id} value={node.value} /> 
+          <Arrow />
+        </>))}  
+     </div>
+     </div>
+);
 }
--- src/components/lnode.jsx
+++ src/components/lnode.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 
-export default function Lnode() {
+export default function Lnode( {value} ) {
     return (
         <div className="mx-1 col-md">
             <div className="rounded-circle slnode row data card-body border border-3 col-md">
-                        <h3 className="text-center">Data</h3>
+                        <h3 className="text-center">{value}</h3>
             </div>
         </div>
     )

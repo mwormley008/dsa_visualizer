08e910a58da25c59df11c07b6773968033d8d3e5
mwormley008 <mwormley008@gmail.com>
2023-04-18 16:28:18 -0500

    delete

--- src/components/linkedlist.jsx
+++ src/components/linkedlist.jsx
@@ -5,14 +5,22 @@ import Arrow from './arrow';
 
 export default function LinkedList(){
   const [nodes, setNodes] = useState([])
-  function handleClick(e){
+  function addNode(e){
       setNodes([...nodes, { id: nodes.length + 1, value: Math.round(Math.random() *10) } ]);
   };
+  
+  function delNode(e){
+      setNodes(nodes => {
+        return nodes.filter(node => node.id !== nodes.length)
+      })
+  }
+  
 
   return ( 
       <div className="container">
         <div className="row justify-content-between">
-        <button onClick={handleClick}>Add Node</button>
+        <button onClick={addNode}>Add Node</button>
+        <button onClick={delNode}>Delete Node</button>
       { nodes.map( node => (
         <> 
           <Lnode key={node.id} value={node.value} /> 

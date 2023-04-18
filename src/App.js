8e30905614b8b9c46ca13385b74e838cc8f506b1
mwormley008 <mwormley008@gmail.com>
2023-04-18 12:37:49 -0500

    add nodes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <div className="App">
             <Navbar />
             <LinkedList /> 
+            <LinkedList /> 
         </div>
     );
 }
--- src/components/linkedlist.jsx
+++ src/components/linkedlist.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Lnode from './lnode';
 import Arrow from './arrow';
 
 export default function LinkedList() {
+
+  const [count, setCount] = useState(1)
+
+  let nodes = []
+  for(let i = 0; i<count; i++) {
+    nodes.push(
+      <> 
+      <Arrow /> <Lnode />
+      </>
+    )
+  }
+
+  function handleClick (e) {
+    setCount(count+1)
+  }
+
   return (
     <div className="container">
       <div className="row justify-content-between">
-        <Lnode />
-        <Arrow />
-        <Lnode />
+        <button onClick={handleClick}>Add Node</button>
+        <Lnode /> 
+        {nodes}
       </div>
     </div>
   )

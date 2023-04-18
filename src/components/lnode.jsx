4aa1cbf976a24713edc626bb3290d8c967bdcff4
mwormley008 <mwormley008@gmail.com>
2023-04-18 13:48:59 -0500

    round cells

--- src/components/lnode.jsx
+++ src/components/lnode.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 export default function Lnode() {
     return (
-        <div className=" mx-1 col-md">
-            <div className="slnode row data card-body border border-3 col-md">
-                        <h3 className="text-center">{Math.round(Math.random()* 10)}</h3>
+        <div className="mx-1 col-md">
+            <div className="rounded-circle slnode row data card-body border border-3 col-md">
+                        <h3 className="text-center">Data</h3>
             </div>
         </div>
     )

942fdf1e0b80afb8178ec414847a119f20965f87
mwormley008 <mwormley008@gmail.com>
2023-04-18 19:32:48 -0500

    null end

--- src/components/linkedlist.jsx
+++ src/components/linkedlist.jsx
@@ -4,11 +4,17 @@ import Arrow from './arrow';
 import { motion, AnimatePresence } from "framer-motion";
 
 export default function LinkedList(){
-  const [nodes, setNodes] = useState([])
+  const [nodes, setNodes] = useState([
+    { id: 'null', value: 'X' },
+  ]);
   const [inputValue, setInputValue] = useState('');
 
   function addNode(e){
-      setNodes([...nodes, { id: nodes.length + 1, value: Math.round(Math.random() *10) } ]);
+    // Create a new node object
+    const newNode = { id: nodes.length + 1, value: Math.round(Math.random() *10) };
+    
+    // Insert the new node as the second to last node in the array
+    setNodes([...nodes.slice(0, -1), newNode, nodes[nodes.length - 1]]);
   };
   
   function delNode(e){
@@ -16,6 +22,12 @@ export default function LinkedList(){
         return nodes.filter(node => node.id !== nodes.length)
       })
   }
+
+  function clearNodes(e){
+    setNodes(nodes => {
+      return nodes.filter(node => node.id > nodes.length)
+    })
+}
 
   function delIdx(e){
     const index = parseInt(inputValue);
@@ -33,22 +45,24 @@ export default function LinkedList(){
       <div className="container">
         <button onClick={addNode}>Add Node</button>
         <button onClick={delNode}>Delete Node</button>
+        <button onClick={clearNodes}>Clear Nodes</button>
         <input type="text" value={inputValue} onChange={handleInputChange}/>
         <button onClick={delIdx}>Delete Index</button>
         <div id="display"> 
           <AnimatePresence>
-            { nodes.map( node => (
+            { nodes.map((node, index) => (
               <motion.div
                 key={node.id}
-                initial={{ opacity: 0, x: -50 }}
-                animate={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 1, x: 0, scale: .5}}
+                animate={{ opacity: 1, x: 0, scale: 1 }}
                 exit={{ opacity: 0, x: 50 }}
                 transition={{ duration: 0.5 }}
+                style={{ display: 'flex', alignItems: 'center' }}
               >
                 <div style={{ width: '75px' }}>
                 <Lnode key={node.id} value={node.value} style={{ width: '75px' }}/> 
                 </div>
-                <Arrow />
+                {index !== nodes.length - 1 && <Arrow />}
               </motion.div>
               ))}  
           </AnimatePresence>
--- src/components/lnode.jsx
+++ src/components/lnode.jsx
@@ -7,9 +7,6 @@ export default function Lnode( {value} ) {
             <div className="rounded-circle slnode row data card-body border border-3 col-md">
                         <h3 className="text-center">{value}</h3>
             </div>
-            <div>
-            <Arrow />
-            </div>
         </div>
     )
 }
